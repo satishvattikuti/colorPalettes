33445a82d6951d4fc7043e274cff128fff80e588
Vattikuti <SATISH_VATTIKUTI1@homedepot.com>
2020-09-15 23:31:37 -0500

    createed project, added seedColors and Palette Component

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* header here  */}
+                <div className="Palette-colors">
+                </div>
+                {/* footer here */}
+            </div>
+        )
+    }
+}
+
